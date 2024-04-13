ee98c0d3286b7e195df17845b51cbc44d63a5cb2
aquilesTrindade <sempedra075@gmail.com>
2024-04-13 13:37:26 -0300

    Re-start Repo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.trindade.stringscreator"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
+        versionCode = 1
         versionName = "@string/app_versionName"
         
         vectorDrawables { 
--- changelog.md
+++ changelog.md
@@ -1,8 +1,6 @@
-# Changelog 2.0
+# Changelog 1.0
 <p>Features</p>
-<h2 2.0 2024/04/13</h2>
+<h2 1.0 2024/04/13</h2>
 <ul>
-<li>Aplication Re-design</li>
-<li>Fragments System Added</li>
-<li>Settings page added</li>
+<li>Nothing, is first version.</li>
 </ul>
