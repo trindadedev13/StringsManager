1328b47ec62744ff6545a176f32127231a255cda
aquilesTrindade <sempedra075@gmail.com>
2024-05-19 02:23:13 -0300

    UI Improvements

--- app/src/main/java/com/trindade/stringscreator/fragments/SettingsFragment.java
+++ app/src/main/java/com/trindade/stringscreator/fragments/SettingsFragment.java
@@ -66,7 +66,7 @@ public class SettingsFragment extends Fragment {
         });
         
         binding.filePicker.setOnClickListener(v -> {
-           
+           testFilePicker();
         });
         
         return binding.getRoot();
